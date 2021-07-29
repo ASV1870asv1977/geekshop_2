42529868c15337d16b69209ed1ef9e8e7d38b134
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-07-29 12:15:33 +0300

    created git repository

--- authapp/models.py
+++ authapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from datetime import datetime
+
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name = 'возраст')
+
+    acttivation_key = models.CharField(max_lenght=128, blank=True)
+    acttivation_key_
